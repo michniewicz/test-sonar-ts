fb7d606f893584731333acfd448c24614cc5d702
michniewicz <kostiantyn.mikhnievych@on-running.com>
2023-07-20 12:27:52 +0200

    smells like teen spirit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 // src/index.ts
 
-// Example 1: Implicit Any
-function add(x: any, y: any) {
-  return x + y;
+function foo1(radius: number): number {
+  const PI = 3.14;
+  return PI * radius * radius;
+}
+
+function foo2(itemPrice: number, quantity: number): number {
+  const discount = 0.1;
+  const subtotal = itemPrice * quantity;
+  const discountedPrice = subtotal - subtotal * discount;
+  return discountedPrice;
+}
+
+function foo3(itemPrice: number, quantity: number): number {
+  const discount = 0.1;
+  const taxRate = 0.08;
+  const subtotal = itemPrice * quantity;
+  const discountedPrice = subtotal - subtotal * discount;
+  const totalPriceWithTax = discountedPrice + discountedPrice * taxRate;
+  return totalPriceWithTax;
 }
 
 
 export function foo(): string {
-  const result = add(5, "10");
-  console.log(result);
+  console.log(foo1(180));
+  console.log(foo2(100, 1));
+  console.log(foo3(100, 3));
 
   return "foo";
 }
